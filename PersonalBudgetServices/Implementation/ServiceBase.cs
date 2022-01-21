d0e1284ef24ee470e4bc011e0f572de6bac6d57e
Vangel Iliev <viliev@sidigitalsolutions.com>
2022-01-21 22:03:22 +0200

    Adding base services

---
+++ PersonalBudgetServices/Implementation/ServiceBase.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PersonalBudgetDb.DbContext;
+using PersonalBudgetServices.Interfaces;
+using PersonalBudgetServices.ServiceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudgetServices.Implementation
+{
+    public abstract class ServiceBase<T> : IServiceBase<T> where T : class, IBaseEntity
+    {
+        protected readonly PersonalBudgetDbContext _dbContext;
+        private readonly ILogger _logger;
+
+        public ServiceBase(PersonalBudgetDbContext dbContext, ILogger logger)
+        {
+            this._dbContext = dbContext;
+            this._logger = logger;
+        }
+
+        public async Task<bool> CreateAsync(T entity)
+        {
+            try
+            {
+                this._dbContext.Set<T>().Add(entity);
+                await this._dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            try
+            {
+                this._dbContext.Set<T>().Remove(entity);
+                await this._dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+            
+        }
+
+        public virtual async Task DeleteAsync(Guid id)
+        {
+            try
+            {
+                var entity = await this._dbContext.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+                this._dbContext.Set<T>().Remove(entity);
+                await this._dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+            
+        }
+
+        public async Task<List<T>> FindAllAsync()
+        {
+            try
+            {
+                return await this._dbContext.Set<T>().AsNoTracking().ToListAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+            
+        }
+
+        public async Task<T> ReadAsync(Guid id)
+        {
+            try
+            {
+                return await this._dbContext.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+            
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            try
+            {
+                this._dbContext.Set<T>().Update(entity);
+                await this._dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+           
+        }
+    }
+}
---
+++ PersonalBudgetServices/Interfaces/IServiceBase.cs
@@ -0,0 +1,24 @@
+﻿using PersonalBudgetServices.ServiceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudgetServices.Interfaces
+{
+    public interface IServiceBase<T> where T : IBaseEntity
+    {
+        Task<List<T>> FindAllAsync();
+
+        Task<bool> CreateAsync(T entity);
+
+        Task<T> ReadAsync(Guid id);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task DeleteAsync(Guid id);
+    }
+}
